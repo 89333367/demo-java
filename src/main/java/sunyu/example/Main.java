3706f5fc4b8e006e0df09a10113ae68435479d68
SunYu <sunyu@uml-tech.com>
2026-01-17 11:08:11 +0800

    项目初始化，增加多环境读取配置功能

---
+++ src/main/java/sunyu/example/Main.java
@@ -0,0 +1,13 @@
+package sunyu.example;
+
+import cn.hutool.log.Log;
+import cn.hutool.log.LogFactory;
+
+public class Main {
+    static Log log = LogFactory.get();
+
+    public static void main(String[] args) {
+        log.info("Hello World!");
+    }
+
+}
---
+++ src/test/java/Tests.java
@@ -0,0 +1,16 @@
+import cn.hutool.log.Log;
+import cn.hutool.log.LogFactory;
+import cn.hutool.setting.dialect.Props;
+import org.junit.jupiter.api.Test;
+import sunyu.example.config.ConfigProperties;
+
+public class Tests {
+    Log log = LogFactory.get();
+    Props props = ConfigProperties.getProps();
+
+    @Test
+    void test() {
+        log.info("hello world");
+    }
+
+}
