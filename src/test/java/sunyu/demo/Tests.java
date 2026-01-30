0276a6081603d68c8d0d1996d564a8c2449e3990
SunYu <sunyu@uml-tech.com>
2026-01-30 14:22:07 +0800

    增加测试用例

--- src/test/java/sunyu/demo/Tests.java
+++ src/test/java/sunyu/demo/Tests.java
@@ -1,16 +1,21 @@
+package sunyu.demo;
+
 import cn.hutool.log.Log;
 import cn.hutool.log.LogFactory;
 import cn.hutool.setting.dialect.Props;
 import org.junit.jupiter.api.Test;
 import sunyu.example.config.ConfigProperties;
+import sunyu.util.RedisClusterUtil;
 
 public class Tests {
     Log log = LogFactory.get();
     Props props = ConfigProperties.getProps();
+    RedisClusterUtil redisClusterUtil = RedisClusterUtil.builder().nodes(props.getStr("redis.cluster.nodes")).build();
 
     @Test
     void test() {
-        log.info("hello world");
+        String value = redisClusterUtil.get("p:r:d:600243");
+        log.info("value: {}", value);
     }
 
 }
