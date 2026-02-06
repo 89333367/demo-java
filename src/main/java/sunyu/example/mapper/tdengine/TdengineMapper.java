436e0ebcbff25a999442fe7d2861101080a79864
SunYu <sunyu@uml-tech.com>
2026-02-06 16:35:28 +0800

    td fix

---
+++ src/main/java/sunyu/example/mapper/tdengine/TdengineMapper.java
@@ -0,0 +1,16 @@
+package sunyu.example.mapper.tdengine;
+
+import org.apache.ibatis.annotations.Param;
+import sunyu.example.config.DS;
+import sunyu.example.entity.DP;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@DS("tdengine")
+public interface TdengineMapper {
+
+    List<DP> selectWorkPoints(@Param("did") String did, @Param("startTime") LocalDateTime startTime, @Param("endTime") LocalDateTime endTime, @Param("checkWorkStatus") Boolean checkWorkStatus);
+    List<DP> selectWorkProtocol(@Param("did") String did, @Param("startTime") LocalDateTime startTime, @Param("endTime") LocalDateTime endTime, @Param("checkWorkStatus") Boolean checkWorkStatus);
+
+}
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,16 +1,34 @@
+import cn.hutool.core.date.LocalDateTimeUtil;
 import cn.hutool.log.Log;
 import cn.hutool.log.LogFactory;
 import cn.hutool.setting.dialect.Props;
 import org.junit.jupiter.api.Test;
 import sunyu.example.config.ConfigProperties;
+import sunyu.example.config.MyBatis;
+import sunyu.example.entity.DP;
+import sunyu.example.mapper.tdengine.TdengineMapper;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class Tests {
     Log log = LogFactory.get();
     Props props = ConfigProperties.getProps();
 
+    List<DP> selectWorkPoints(String did, LocalDateTime startTime, LocalDateTime endTime) {
+        TdengineMapper mapper = MyBatis.getMapper(TdengineMapper.class);
+        List<DP> l = mapper.selectWorkPoints(did, startTime, endTime, false);
+        return l;
+    }
+
     @Test
     void test() {
-        log.info("hello world");
+        String did = "LFS1032311100736";
+        String startTime = "20260116104103";
+        String endTime = "20260116153830";
+        double jobWidth = 3.5;
+        List<DP> l = selectWorkPoints(did, LocalDateTimeUtil.parse(startTime, "yyyyMMddHHmmss"), LocalDateTimeUtil.parse(endTime, "yyyyMMddHHmmss"));
+        log.info("{}", l.size());
     }
 
 }
